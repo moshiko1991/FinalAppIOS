256eb784996f7db8da5231322ace509c3dc60ef9
moshiko1991 <moshikoelkalay@gmail.com>
2020-02-29 15:10:10 +0200

    Initial Commit

---
+++ FinalAppIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalAppIOS
+//
+//  Created by moshiko elkalay on 29/02/2020.
+//  Copyright © 2020 moshiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
